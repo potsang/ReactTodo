7f2cec8dbaa9e9d282b1b087f5eff5faa499529b
Potsang Huang <freelancer5858@yahoo.com>
2017-02-23 21:00:43 -0600

    add completed and created timestamp

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const uuid = require('uuid');
+const moment = require('moment');
 
 const TodoList = require('TodoList');
 const AddTodo = require('AddTodo');
@@ -24,7 +25,9 @@ var TodoApp = React.createClass({
         {
           id: uuid(),
           text: text,
-          completed: false
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
         }
       ]
     });
@@ -33,6 +36,7 @@ var TodoApp = React.createClass({
     var updatedTodos = this.state.todos.map((todo) => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
       }
       return todo;
     });
---
+++ playground/moment-examples.js
@@ -0,0 +1,12 @@
+const moment = require('moment');
+
+console.log(moment().format());
+
+var now = moment();
+console.log('Current timestamp', now.unix());
+
+var timestamp = 1487901033;
+var currentMoment = moment.unix(timestamp);
+console.log('current moment', currentMoment.format('MMM D, YY @ h:mm a'));
+
+console.log('current moment', currentMoment.format('MMMM Do, YYYY @ h:mm A'));
