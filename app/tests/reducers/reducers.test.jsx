754fb201ebd3ccf95e84b85556e4b109f2d76ddc
Potsang Huang <freelancer5858@yahoo.com>
2017-03-05 18:30:33 -0600

    add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,37 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todoReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todoReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: '123',
+          text: 'Walk the dog',
+          completed: true,
+          createdAt: 12345,
+          completedAt: 123456
+        }
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      var res = reducers.todoReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toNotExist();
+    });
+  });
 });
