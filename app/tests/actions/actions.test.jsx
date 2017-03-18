bdbcf9a6dc25a4d1f956b4649de8c35030237a87
Potsang Huang <freelancer5858@yahoo.com>
2017-03-18 18:26:16 -0500

    add auth reducer and actions

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -86,6 +86,25 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate login action', () => {
+    var action = {
+      type: 'LOGIN',
+      uid: '123abc'
+    };
+    var res = actions.login(action.uid);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate logout action', () => {
+    var action = {
+      type: 'LOGOUT'
+    };
+    var res = actions.logout();
+
+    expect(res).toEqual(action);
+  });
+
   describe('Tests with firebase todos', () => {
     var testTodoRef;
 
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,6 +27,30 @@ describe('Reducers', () => {
     });
   });
 
+  describe('authReducer', () => {
+    it('should store uid on login', () => {
+      var action = {
+        type: 'LOGIN',
+        uid: '123abc'
+      };
+      var res = reducers.authReducer(undefined, df(action));
+
+      expect(res).toEqual({uid: action.uid});
+    });
+
+    it('should wipe auth on logout', () => {
+      const authData = {
+        uid: '123abc'
+      };
+      var action = {
+        type: 'LOGOUT'
+      };
+      var res = reducers.authReducer(df(authData), df(action));
+
+      expect(res).toEqual({});
+    });
+  });
+
   describe('todosReducer', () => {
     it('should add new todo', () => {
       var action = {
